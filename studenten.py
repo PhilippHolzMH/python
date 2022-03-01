eb94ced2293fa4a6e8d7e0187ff8915537cbd64d
Philipp Holz <philippwood@gmx.net>
2022-03-01 17:11:55 +0100

    deleted item

--- studenten.py
+++
@@ -1,8 +0,0 @@
-import csv
-from csv import reader
-with open ('studentendb.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row['vorname'], row['nachname'])
-
-print(row)
