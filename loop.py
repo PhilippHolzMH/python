ee7cdca49b173e97e2843373f02b3a6de98879d9
Philipp Holz <philippwood@gmx.net>
2022-03-01 10:39:39 +0100

    init python

---
+++ loop.py
@@ -0,0 +1,14 @@
+import random
+print ("Welcome to MWP - magic with Philipp")
+print ("The rules are complex. I will think of a number and you will try to guess it")
+number = random.randint(1,10)
+
+isguessrigth = False
+while isguessrigth != True:
+    guess = input ("Samma ne nummer von 1 - 10")
+    if int(guess) == number:
+        print ("Du hast {} genommen. Dat is richtig. Gewonnen!".format(guess))
+        isguessrigth = True
+    else:
+        print("Deine Zahl ist {}. Scusi - is falsch".format(guess))
+        
