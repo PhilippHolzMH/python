c43a657877341a837efcecbda6aa0ea09e0a85ee
Philipp Holz <philippwood@gmx.net>
2022-03-02 13:06:30 +0100

    while added

--- module_os/folder.py
+++ module_os/folder.py
@@ -1,5 +1,10 @@
 import os
 def create_fold (foldername,inputprofile):
-    os.mkdir(foldername)
-    os.system("touch "+ foldername +"/profile.txt")
-    os.system("echo "+ inputprofile +" >> "+ foldername +"/profile.txt") 
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+        os.system("touch "+ foldername +"/profile.txt")
+        os.system("echo "+ inputprofile +" >> "+ foldername +"/profile.txt") 
+        return True
+    else: 
+        return False
+        
--- module_os/main.py
+++ module_os/main.py
@@ -1,10 +1,14 @@
 from birthday import berechnung_alter
 from folder import create_fold
+success = False
 
-nachname = input("So gebe er mir den Nachnamen: ")
-tag,monat,jahr = map(int, input("Wann hat er das Licht der Welt entdeckt? ").split('.'))
-alter = berechnung_alter (jahr, monat, tag)
-if alter > 17:
-    create_fold (nachname + str(alter), str(alter))
-else:
-    print ("Sorry du bist zu jung")
+while success == False:
+    nachname = input("So gebe er mir den Nachnamen: ")
+    tag,monat,jahr = map(int, input("Wann hat er das Licht der Welt entdeckt? ").split('.'))
+    alter = berechnung_alter (jahr, monat, tag)
+
+    if alter > 17:
+
+        success = create_fold (nachname + str(alter), str(alter))
+    else:
+        print ("Sorry du bist zu jung")
