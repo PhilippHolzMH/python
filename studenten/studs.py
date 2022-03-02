291930e132aa43fedc49e14dbd4ba10093c037d9
Philipp Holz <philippwood@gmx.net>
2022-03-02 12:40:37 +0100

    aufräumen

---
+++ studenten/studs.py
@@ -0,0 +1,22 @@
+import csv
+from datetime import date
+from ntpath import join
+today = date.today()
+def berechnung_alter(geburtsjahr, geburtsmonat, geburtstag):
+    
+    diff = today.year - geburtsjahr
+    hattegeb = (today.month, today.day) < (geburtsmonat, geburtstag)
+    if hattegeb : 
+        return diff -1
+    return diff
+
+vorname,nachname = map(str,input("So gebe er mir Vor und Nachnamen: ").split ())
+tag,monat,jahr = map(int, input("Wann hat er das Licht der Welt entdeckt? ").split('.'))
+alter = berechnung_alter (jahr, monat, tag)
+
+if alter > 18:
+    with open ('studentendb.csv', 'a', newline='') as input_file:
+        writer = csv.writer(input_file)
+        writer.writerow([vorname,nachname,alter])
+else:
+    print ("Sorry du bist zu jung")
