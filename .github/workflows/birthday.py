63ea04b77ef96178b4c618ef8791c01e9f1a15d9
PhilippHolzMH <98755815+PhilippHolzMH@users.noreply.github.com>
2022-03-03 16:03:02 +0100

    Delete birthday.py

--- .github/workflows/birthday.py
+++
@@ -1,8 +0,0 @@
-def berechnung_alter(birthdate, today):
-    
-    diff = today.year - birthdate.year
-    hattegeb = (today.month, today.day) < (birthdate.month, birthdate.day)
-    if hattegeb : 
-        return diff -1
-    return diff
-
